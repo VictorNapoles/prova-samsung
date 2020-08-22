64bf83fb13277f930d794030c637296bca7af240
Victor de Nápoles <victor.avansys@gmail.com>
2020-08-22 20:03:17 -0300

    Ajuste de warning

--- samsung-evaluation-ui/src/app/app.module.ts
+++ samsung-evaluation-ui/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { NgxMaskModule, IConfig } from 'ngx-mask'
+import { NgxMaskModule, IConfig } from 'ngx-mask';
 
 
 import { AppRoutingModule } from './app-routing.module';
